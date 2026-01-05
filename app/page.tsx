d71f9abf09a9a547f9c680a5628d17e1a4c74e93
Hieu Phan <hphan.tech@gmail.com>
2026-01-05 22:18:46 +0100

    nextjs16 complying

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,36 @@
-import ExploreBtn from '@/components/ExploreBtn'
-import NavBar from '@/components/NavBar'
-import React from 'react'
-import EventCard from '@/components/EventCard'
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
-import { IEvent } from '@/database/event.model'
-import { cacheLife } from 'next/cache'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
-const page = async () => {
-  'use cache';
-  cacheLife('hours')
+            <ExploreBtn />
 
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
 
-  let events = [];
-  try {
-    const response = await fetch(`${BASE_URL}/api/events`, { cache: 'no-store' });
-    if (response.ok) {
-      const data = await response.json();
-      events = data.events ?? [];
-    }
-  } catch (error) {
-    console.error("Failed to fetch events:", error);
-  }
-
-  return (
-    <section>
-      <h1 className='text-center'>Welcome to Our Event Platform <br /> find events you can't miss</h1>
-      <p className='text-center mt-5'>Hackathons, meetups, and conferences all in one place</p>
-
-      <ExploreBtn />
-      
- 
-      <div className='mt-20 space-y-7'>
-        <h3>Featured Events</h3>
-
-        <ul className='events'>
-            {events && events.length > 0 && events.map((event : IEvent) => (
-              <li key={event.title} className='list-none'>
-                <EventCard {...event} />
-
-              </li>
-
-              ))}
-            
-
-        </ul>
-
-
-      </div>
-    </section>
-  )
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
 }
 
-export default page
+export default Page;
