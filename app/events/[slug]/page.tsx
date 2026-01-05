1fa81d28ffe68adc795ab7678728af49e21cb03c
Hieu Phan <hphan.tech@gmail.com>
2026-01-06 00:13:45 +0100

    pls

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -2,9 +2,11 @@ import { Suspense } from "react";
 import { notFound } from "next/navigation";
 import EventDetails from "@/components/EventDetails";
 
-export const dynamic = "force-dynamic";
-
-export default function EventDetailsPage({ params }: { params: { slug?: string } }) {
+export default function EventDetailsPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const slug = params?.slug;
 
   if (!slug) return notFound();
